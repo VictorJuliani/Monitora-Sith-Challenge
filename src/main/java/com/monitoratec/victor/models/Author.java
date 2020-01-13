aa6770b9170ad3a91b97f90a599b4b071830c94e
Victor Juliani <v_juliani@hotmail.com>
2020-01-13 16:52:45 -0300

    Create extra constructors for Author

--- src/main/java/com/monitoratec/victor/models/Author.java
+++ src/main/java/com/monitoratec/victor/models/Author.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -13,7 +14,9 @@ import java.time.LocalDate;
 @Data
 @Entity
 public class Author {
-    private @Id @GeneratedValue int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     @NotEmpty
     private String firstName;
     @NotEmpty
@@ -24,4 +27,16 @@ public class Author {
     private boolean distinguished;
 
     public Author() {}
+
+    public Author(@NotEmpty String firstName, @NotEmpty String lastName, @NotNull @Past LocalDate birthdate, @NotNull boolean distinguished) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.distinguished = distinguished;
+    }
+
+    public Author(int id, @NotEmpty String firstName, @NotEmpty String lastName, @NotNull @Past LocalDate birthdate, @NotNull boolean distinguished) {
+        this(firstName, lastName, birthdate, distinguished);
+        this.id = id;
+    }
 }
