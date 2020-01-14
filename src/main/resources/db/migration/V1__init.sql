0a3de0b270305546e4a9a4a780837071c527b9d6
Victor Juliani <v_juliani@hotmail.com>
2020-01-14 18:15:19 -0300

    Small syntax update for primary keys on SQL files

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,7 @@
 CREATE TABLE authors (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   first_name varchar(50) NOT NULL,
   last_name varchar(50) NOT NULL,
   birthdate date NOT NULL,
-  distinguished boolean NOT NULL DEFAULT false,
-  PRIMARY KEY (id)
+  distinguished boolean NOT NULL DEFAULT false
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
