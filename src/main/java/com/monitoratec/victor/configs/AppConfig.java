adf569a1a19b5c5a7fd228b9f76b51ce93937ce0
Victor Juliani <v_juliani@hotmail.com>
2020-01-16 09:49:22 -0300

    Rename JSONParserConfig to AppConfig

--- src/main/java/com/monitoratec/victor/configs/AppConfig.java
+++ src/main/java/com/monitoratec/victor/configs/AppConfig.java
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import java.time.format.DateTimeFormatter;
 
 @Configuration
 @SuppressWarnings("unused")
-public class JSONParserConfig {
+public class AppConfig {
     private static final String dateTimeFormat = "yyyy-MM-dd'T'HH:mm'Z'";
 
     @Bean
